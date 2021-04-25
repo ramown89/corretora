1afcf78a2239b808121446abd8ffbfcc33ad5c71
Ramon Lima <ramown.dev89@gmail.com>
2021-04-25 16:07:28 -0300

    apresentação de página estilizada

--- app/controllers/home/views.py
+++ app/controllers/home/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 from . import home
 
 
 @home.route('/')
 def home_page():
-	return "Hello World - NEW"
+	return render_template('home/index.html', title="Home Page")
